c3252efe4e7f4c0df717e727482b9d81c3496d86
qiaosungithub <sqa23@mails.tsinghua.edu.cn>
2025-11-17 15:24:29 -0500

    fix wandb key

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ now=`date '+%Y%m%d_%H%M%S'`
 JOBNAME=sqa_Qwen_LoRA_${now}
 LOGDIR=$HERE/logs/$JOBNAME
 
-export WANDB_API_KEY=73f8ff40bb7f8589e9bd1f476196a896f662cdfa # zhh's wandb key
+export WANDB_API_KEY=73f8ff40bb7f8589e9bd1f476196a896f662cdfa # sqa's wandb key
 
 mkdir -p ${LOGDIR}
 # sudo chmod 777 -R ${LOGDIR}
